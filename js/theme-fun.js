969947dece1059eeff6fc4a2d93052b963e61a60
zubair894 <shahzubair269@gmail.com>
2020-09-20 23:20:28 +0500

    first commit

---
+++ js/theme-fun.js
@@ -0,0 +1,198 @@
+ $('.main-slider').slick({
+        infinite: true,
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        fade:true,
+        speed:2500,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        pauseOnHover: false,
+        responsive: [
+            {
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 1,
+                    dots: true,
+                    slidesToScroll: 1,
+                    arrows:true
+                }
+            },
+            {
+                breakpoint: 560,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: true,
+                    arrows:true
+                }
+            }
+        ]
+        
+    });
+
+    $('.about-slider').slick({
+        infinite: true,
+        dots: true,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: false,
+        autoplay: true,
+        speed:1500,
+        autoplaySpeed: 2000,
+        pauseOnHover: false,
+        responsive: [
+            {
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 1,
+                    dots: true,
+                    slidesToScroll: 1,
+                    arrows:true
+                }
+            },
+            {
+                breakpoint: 560,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: true,
+                    arrows:true
+                }
+            }
+        ]
+        
+    });
+
+
+
+    $('.product-slider').slick({
+        infinite: true,
+        dots: false,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        arrows: true,
+        autoplay: true,
+        autoplaySpeed: 2000,
+        pauseOnHover: false,
+        responsive: [
+            {
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 1,
+                    dots: true,
+                    slidesToScroll: 1,
+                    arrows:true
+                }
+            },
+            {
+                breakpoint: 560,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: true,
+                    arrows:true
+                }
+            }
+        ]
+        
+    });
+
+    $('.party-slider').slick({
+        infinite: true,
+        dots: false,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: true,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        pauseOnHover: false,
+        responsive: [
+            {
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 1,
+                    dots: true,
+                    slidesToScroll: 1,
+                    arrows:true
+                }
+            },
+            {
+                breakpoint: 560,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: true,
+                    arrows:true
+                }
+            }
+        ]
+        
+    });
+
+
+
+    $('.department-slider').slick({
+        infinite: true,
+        dots: false,
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: true,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        pauseOnHover: false,
+        responsive: [
+            {
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 1,
+                    dots: true,
+                    slidesToScroll: 1,
+                    arrows:true
+                }
+            },
+            {
+                breakpoint: 560,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: true,
+                    arrows:true
+                }
+            }
+        ]
+        
+    });
+
+    $('.booking-slider').slick({
+        infinite: true,
+        dots: false,
+        slidesToShow: 2,
+        slidesToScroll: 1,
+        arrows: true,
+        autoplay: false,
+        autoplaySpeed: 2000,
+        pauseOnHover: false,
+        responsive: [
+            {
+                breakpoint: 991,
+                settings: {
+                    slidesToShow: 1,
+                    dots: true,
+                    slidesToScroll: 1,
+                    arrows:true
+                }
+            },
+            {
+                breakpoint: 560,
+                settings: {
+                    slidesToShow: 1,
+                    slidesToScroll: 1,
+                    dots: true,
+                    arrows:true
+                }
+            }
+        ]
+        
+    });
